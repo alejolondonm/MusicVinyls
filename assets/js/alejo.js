1865339e3ddc659fb437039469b2f2153528a9f5
Alejo <luis.londono@upb.edu.co>
2023-02-26 10:08:02 -0500

    Implementación localstorage 2

--- assets/js/alejo.js
+++ assets/js/alejo.js
@@ -1,3 +1,10 @@
+let imagen, btn_cerrar, titulo, mensaje;
+
+function toLogin() {
+    location.href = "login.html";
+}
+
+
 function registro(e) {
     event.preventDefault();
 
@@ -10,19 +17,26 @@ function registro(e) {
     var password = document.getElementById('clave1').value;
     var password2 = document.getElementById('clave2').value;
 
-    //Creación del usuario
-    var myUser = {
-        nombre: nombre,
-        apellido: apellido,
-        user: user,
-        email: email,
-        celular: celular,
-        password: password,
-    };
+    if (password == password2) {
+        //Creación del usuario
+        var myUser = {
+            nombre: nombre,
+            apellido: apellido,
+            user: user,
+            email: email,
+            celular: celular,
+            password: password,
+        };
 
-    var json = JSON.stringify(myUser);
-    localStorage.setItem(user, json);
-    console.log('¡Usuario creado con éxito!');
+        var json = JSON.stringify(myUser);
+        localStorage.setItem(user, json);
+        console.log('¡Usuario creado con éxito!');
+        alert('¡Usuario creado con éxito!');
+        location.href = "login.html";
+    }
+    else {
+        alert('Revisa los datos');
+    }
 
 }
 
@@ -30,11 +44,23 @@ function registro(e) {
 function loginFunc(e) {
     event.preventDefault();
 
-    var user = document.getElementById('user').value;
-    var password = document.getElementById('clave1').value;
+    var user = document.getElementById('userlog').value;
+    var password = document.getElementById('clavelog').value;
+    var result = document.getElementById('result');
 
-    var username = localStorage.getItem(myUser);
+    var username = localStorage.getItem(user);
     var data = JSON.parse(username);
     console.log(data);
 
+    /*
+    if (username == null) {
+        result.innerHTML = 'Usuario incorrecto';
+    }
+    else if (user == data.user && password == data.password) {
+        location.href = 'index.html';
+    }
+    else {
+        result.innerHTML = 'Contraseña incorrecta';
+    }
+    */
 }
