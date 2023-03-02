67aa2a8ed1a93dadd3bf5685091b3a1928632fe7
Alejo <luis.londono@upb.edu.co>
2023-03-02 16:43:59 -0500

    Corrección Registro

--- assets/js/alejo.js
+++ assets/js/alejo.js
@@ -1,3 +1,9 @@
+let frm_login, frm_registro, home;
+let btn_ir_a_registro, btn_enviar_registrar, btn_enviar;
+let correo;
+let img_perfil = "https://icons.iconarchive.com/icons/saviourmachine/chat/48/music-icon.png";
+
+
 function toBlog() {
     location.href = "blog.html";
 }
@@ -37,8 +43,6 @@ function toAPI() {
     location.href = "spotifyapi/playlist.html";
 }
 
-
-
 function registro(e) {
     event.preventDefault();
 
@@ -51,7 +55,7 @@ function registro(e) {
     var password = document.getElementById('clave1').value;
     var password2 = document.getElementById('clave2').value;
 
-    if (nombre.length > 5 && nombre.length < 20) {
+    /*[if (nombre.length > 5 && nombre.length < 20) {
         alert('El nombre debe tener entre 5 y 20 caracteres');
     }
     else if (length(apellido) > 5 && length(apellido) < 20) {
@@ -60,23 +64,27 @@ function registro(e) {
     else if (length(user) > 8 && length(user) < 15) {
         alert('El usuario debe tener entre 8 y 15 caracteres');
     }
-    else {
-        //Creación del usuario
-        var myUser = {
-            nombre: nombre,
-            apellido: apellido,
-            user: user,
-            email: email,
-            celular: celular,
-            password: password,
-        };
+    else {*/
 
-        var json = JSON.stringify(myUser);
-        localStorage.setItem(user, json);
-        console.log('¡Usuario creado con éxito!');
-        alert('¡Usuario creado con éxito!');
-        location.href = "login.html";
-    }
+
+    //Creación del usuario
+    var myUser = {
+        nombre: nombre,
+        apellido: apellido,
+        user: user,
+        email: email,
+        celular: celular,
+        password: password,
+    };
+
+    var json = JSON.stringify(myUser);
+    localStorage.setItem(user, json);
+    console.log('¡Usuario creado con éxito!');
+    alert('¡Usuario creado con éxito!');
+    location.href = "login.html";
+
+
+    //}
 
 }
 
