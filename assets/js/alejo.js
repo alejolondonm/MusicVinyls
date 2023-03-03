b17677ad3716bc7b679de288dfd3b65c6511f09b
Alejo <luis.londono@upb.edu.co>
2023-03-03 00:27:33 -0500

    Corrección Registro y login 2

--- assets/js/alejo.js
+++ assets/js/alejo.js
@@ -3,59 +3,192 @@ let btn_ir_a_registro, btn_enviar_registrar, btn_enviar;
 let correo;
 let img_perfil = "https://icons.iconarchive.com/icons/saviourmachine/chat/48/music-icon.png";
 
+window.onload = init; window.onload = init;
 
-function toBlog() {
-    location.href = "blog.html";
+function init() {
+    console.log("Page loaded")
+    hideElementIfLogged();
 }
 
-function toCollection() {
-    location.href = "collection.html";
+function init() {
+    func_ocultar();
 }
 
-function toIndex() {
-    location.href = "index.html";
+//Registrar usuarios
+function registrar() {
+
+    let miUser;
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let usuario = document.getElementById("usuario");
+    let celular = document.getElementById("celular");
+    let correo = document.getElementById("correo1");
+    let genero = document.getElementById("genero");
+    let clave = document.getElementById("clave1");
+    let clave2 = document.getElementById("clave2");
+
+    if (clave == clave2) {
+        miUser = {
+            nombre: nombre.value,
+            apellido: apellido.value,
+            usuario: usuario.value,
+            celular: celular.value,
+            correo: correo.value,
+            genero: genero.value,
+            clave: clave.value
+        };
+        localStorage.setItem("usuario", JSON.stringify(miUser));
+        alert("¡Muy bien, registro exitoso!");
+        //cambiarFormulario();
+    }
+    else {
+        alert("Revisa los campos");
+    }
+
 }
 
-function toNewArrivals() {
-    location.href = "newArrivals.html";
+//Función activada con el submit
+const form_registro = document.getElementById('form_registro');
+
+
+try {
+    if (form_registro) {
+        form_registro.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            registrar();
+            alert("Registrado con éxito!")
+            window.location.href = 'index.html';
+        });
+    }
+} catch (error) {
+    console.log(error);
 }
 
-function toPromos() {
-    location.href = "promos.html";
+//PARA EL LOGIN
+//Función activada con el submit
+const login_form = document.getElementById('frm_login');
+
+if (login_form) {
+    login_form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let user1 = document.getElementById('usuario').value;
+        let psw = document.getElementById('clave').value;
+        //let hashedPass = md5(uPassword);
+
+        login(user1, psw);
+    });
 }
 
-function toLogin() {
-    location.href = "login.html";
+function login(username, clave) {
+
+    let usuario_ls = localStorage.getItem("usuario");
+
+    let user = JSON.parse(usuario_ls);
+
+    if ((user.usuario === username && user.clave === clave)) {
+        localStorage.setItem("logueado", true);
+        alert('Bienvenido ${user.nombre}');
+        window.location.href = "index.html";
+    } else {
+        alert("Revise los datos");
+    }
 }
 
-function toRegistro() {
-    location.href = "registrate.html";
+
+const inicio = document.getElementById('iniciosesion');
+const cierre = document.getElementById('cierresesion');
+
+const navigateTo = (url) => {
+    window.location.href = url;
+};
+
+inicio.addEventListener('click', () => {
+    navigateTo('login.html');
+});
+
+cierre.addEventListener('click', () => {
+    navigateTo('index.html');
+});
+
+
+// Comprobación de logueo de un usuario
+// En este caso, será del API de Spotify
+document.addEventListener('DOMContentLoaded', function () {
+    const apiSpotify = document.getElementById('miapi');
+
+    let siLog = localStorage.getItem("logueado");
+
+    if (apiSpotify) {
+        apiSpotify.addEventListener('click', () => {
+            if (logueado === 'true') {
+                window.location.href = 'spotifyapi/playlist.html';
+            } else {
+                alert('Debes crear una cuenta para buscar playlists')
+                return window.location.href = 'register.html';
+            }
+        })
+    }
+
+});
+
+
+//Para ocultar el botón de inicio sesión
+function func_ocultar() {
+    if (localStorage.getItem('logueado') === 'true') {
+        const showElement = document.getElementById('cierresesion')
+        const elementToHide = document.getElementById('iniciosesion');
+        elementToHide.style.display = 'none';
+        showElement.style.display = 'block';
+    } else {
+        const showElement = document.getElementById('cierresesion');
+        if (showElement) showElement.style.display = 'none';
+
+    }
 }
 
-function toVinyls() {
-    location.href = "vinyls.html";
-}
+//Cerrar Sesión
+const showElement = document.getElementById('cierresesion')
 
-function toAPI() {
-    //let isLogged = localStorage.getItem("isLogged");
+showElement ? showElement.addEventListener('click', () => {
+    localStorage.removeItem('logueado');
 
-    //if (isLogged === 'true')
-    location.href = "spotifyapi/playlist.html";
-}
+    return window.location.href = 'index.html';
 
+}) : false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 window.onload = function () {
     frm_login = document.getElementById("frm_login");
     btn_enviar = document.getElementById("btn_enviar");
     btn_enviar_registrar = document.getElementById("btn_enviar_registrar");
     frm_registro = document.getElementById("frm_registro");
-    home = document.getElementById("vinilos");
+    home = document.getElementById("home");
     btn_ir_a_registro = document.getElementById("btn_ir_a_registro");
     btn_ir_a_registro.addEventListener("click", irARegistro);
-    //btn_enviar.addEventListener("click",validar);
-    //btn_enviar_registrar.addEventListener("click",registrar);
+    btn_enviar.addEventListener("click", validar);
+    btn_enviar_registrar.addEventListener("click", registrar);
     configurar_login();
     configurar_registro();
 }
+*/
 
 
 function configurar_login() {
@@ -102,13 +235,15 @@ function compararClave() {
         frm_login.classList.add("ocultar");
         home.classList.remove("ocultar");
         html = `
-      <nav>
-        <img src="${img_perfil}" />
-        <a href="javascript:void(0);" id="btn_cerrar_sesion" onclick="cerrarSesion();">Cerrar sesion</a>
-      </nav>
-      <h2>Pagina principal </h2> 
-      <br>Hola ${usuario.nombre}
-     `;
+        <span>
+            <img src="assets/img/gallery/male.jpeg" alt="">
+        </span>
+        <form class="d-flex">
+            <a class="text-1000" href="login.html">Cerrar Sesión</a>
+            <a href="javascript:void(0)" id="btn_cerrar_sesion" 
+                onClick="cerrarSesion();" class="btn">
+        </form>
+        `;
         home.innerHTML = html;
     }
     else {
@@ -121,31 +256,54 @@ function cerrarSesion(event) {
     home.classList.add("ocultar");
 }
 
-function registrar() {
 
-    let nombre = document.getElementById("nombre");
-    let apellido = document.getElementById("apellido");
-    let celular = document.getElementById("celular");
-    let direccion = document.getElementById("direccion");
-    let correo = document.getElementById("correo1");
-    let clave = document.getElementById("clave1");
-    let usuario = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        celular: celular.value,
-        direccion: direccion.value,
-        correo: correo.value,
-        clave: clave.value
-    };
 
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-    alert("¡Muy bien, registro exitoso!")
-    cambiarFormulario();
+
+
+
+
+
+function toBlog() {
+    location.href = "blog.html";
+}
+
+function toCollection() {
+    location.href = "collection.html";
+}
+
+function toIndex() {
+    location.href = "index.html";
+}
+
+function toNewArrivals() {
+    location.href = "newArrivals.html";
+}
+
+function toPromos() {
+    location.href = "promos.html";
+}
+
+function toLogin() {
+    location.href = "login.html";
+}
+
+function toRegistro() {
+    location.href = "registrate.html";
+}
+
+function toVinyls() {
+    location.href = "vinyls.html";
+}
+
+function toAPI() {
+    //let isLogged = localStorage.getItem("isLogged");
+
+    //if (isLogged === 'true')
+    location.href = "spotifyapi/playlist.html";
 }
 
 
-
-
+/*
 function registro(e) {
     event.preventDefault();
 
@@ -167,29 +325,30 @@ function registro(e) {
     else if (length(user) > 8 && length(user) < 15) {
         alert('El usuario debe tener entre 8 y 15 caracteres');
     }
-    else {*/
+    else {
 
 
-    //Creación del usuario
-    var myUser = {
-        nombre: nombre,
-        apellido: apellido,
-        user: user,
-        email: email,
-        celular: celular,
-        password: password,
-    };
+//Creación del usuario
+var myUser = {
+    nombre: nombre,
+    apellido: apellido,
+    user: user,
+    email: email,
+    celular: celular,
+    password: password,
+};
 
-    var json = JSON.stringify(myUser);
-    localStorage.setItem(user, json);
-    console.log('¡Usuario creado con éxito!');
-    alert('¡Usuario creado con éxito!');
-    location.href = "login.html";
+var json = JSON.stringify(myUser);
+localStorage.setItem(user, json);
+console.log('¡Usuario creado con éxito!');
+alert('¡Usuario creado con éxito!');
+location.href = "login.html";
 
 
     //}
 
 }
+
 
 //Función para el login
 function loginFunc(e) {
@@ -232,3 +391,5 @@ function cerrarSesion() {
 
     }
 }
+
+*/
